ac29821aff2e0be9bb53c410cd2463f1ed1bb2fd
pr-citrate <pr.citrate@gmail.com>
2023-02-21 03:08:14 +0900

    adding Board class

--- 01-2048/main.py
+++ 01-2048/main.py
@@ -1,113 +1,69 @@
-# from dataclasses import dataclass, field
+from random import randrange
 
-# background #bbada0
+class Board:
+    size = 4
+    def __init__(self) -> None:
+        self.board = [ [ 0  for _ in range(Board.size) ] \
+                     for _ in range(Board.size) ]
+    
+    def get_blank(self) -> list[tuple[int, int]]:
+        blanks = []
+        for i in self.board:
+            for j in i:
+                if j == 0:
+                    blanks.append((i, j))
+        return blanks
+    
+    def spawn_tile(self) -> None:
+        num = 2 if randrange(10) != 0 else 4
+        idx = self.get_blank()
+        self.board[idx[0]][idx[1]] = num
+        
+    def game_over(self) -> bool:
+        for i in range(Board.size):
+            prevr = 0
+            prevc = 0
+            for j in range(Board.size):
+                if self.board[i][j] == prevc or self.board[j][i] == prevr or self.board[i][j] == 0:
+                    return True
+                prevc, prevr = self.board[i][j], self.board[j][i]
+        return False
+    
 
-# empty #CCC0B4
-
-# .tile.tile-2 .tile-inner {
-#   background: #eee4da;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0), inset 0 0 0 1px rgba(255, 255, 255, 0);
-# }
-
-# .tile.tile-4 .tile-inner {
-#   background: #eee1c9;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0), inset 0 0 0 1px rgba(255, 255, 255, 0);
-# }
-
-# .tile.tile-8 .tile-inner {
-#   color: #f9f6f2;
-#   background: #f3b27a;
-# }
-
-# .tile.tile-16 .tile-inner {
-#   color: #f9f6f2;
-#   background: #f69664;
-# }
-
-# .tile.tile-32 .tile-inner {
-#   color: #f9f6f2;
-#   background: #f77c5f;
-# }
-
-# .tile.tile-64 .tile-inner {
-#   color: #f9f6f2;
-#   background: #f75f3b;
-# }
-
-# .tile.tile-128 .tile-inner {
-#   color: #f9f6f2;
-#   background: #edd073;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.238095), inset 0 0 0 1px rgba(255, 255, 255, 0.142857);
-#   font-size: 45px;
-# }
-
-# .tile.tile-256 .tile-inner {
-#   color: #f9f6f2;
-#   background: #edcc62;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.31746), inset 0 0 0 1px rgba(255, 255, 255, 0.190476);
-#   font-size: 45px;
-# }
-
-# .tile.tile-512 .tile-inner {
-#   color: #f9f6f2;
-#   background: #edc950;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.396825), inset 0 0 0 1px rgba(255, 255, 255, 0.238095);
-#   font-size: 45px;
-# }
-
-# @media screen and (max-width: 520px) {
-#   .tile.tile-512 .tile-inner {
-#     font-size: 25px;
-#   }
-# }
-
-# .tile.tile-1024 .tile-inner {
-#   color: #f9f6f2;
-#   background: #edc53f;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.47619), inset 0 0 0 1px rgba(255, 255, 255, 0.285714);
-#   font-size: 35px;
-# }
-
-# @media screen and (max-width: 520px) {
-#   .tile.tile-1024 .tile-inner {
-#     font-size: 15px;
-#   }
-# }
-
-# .tile.tile-2048 .tile-inner {
-#   color: #f9f6f2;
-#   background: #edc22e;
-#   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.555556), inset 0 0 0 1px rgba(255, 255, 255, 0.333333);
-#   font-size: 35px;
-# }
-
-# @media screen and (max-width: 520px) {
-#   .tile.tile-2048 .tile-inner {
-#     font-size: 15px;
-#   }
-# }
-
-# .tile.tile-super .tile-inner {
-#   color: #f9f6f2;
-#   background: #3c3a33;
-#   font-size: 30px;
-# }
 
 def main():
+    style = {
+        "background" : "#bbada0",
+        "empty" : "#CCC0B4",
+        2 : "#eee4da",
+        4 : "#eee1c9",
+        8 : "#f3b27a",
+        16 : "#f69664",
+        32 : "#f77c5f",
+        64 : "#f75f3b",
+        128 : "#edd073",
+        256 : "#edcc62",
+        512 : "#edc950",
+        1024 : "#edc53f",
+        2048 : "#edc22e",
+        "super" : "#3c3a33"
+    }
+    
+    # main function
     while True:
-        init_game()
+        score, board, direction = init_game()
         while True:
-            spawn_tile()
-            display_game()
-            if game_over():
+            board.spawn_tile()
+            display_game(board)
+            if board.game_over():
                 break
-            while not valid_direction():
-                if restart():
+            while not board.valid_direction():
+                if restart(direction):
                     break
-                get_direction()
+                direction = get_direction()
             else:
-                swipe()
-        alert_death()
+                board.swipe(direction)
+        alert_death(score)
         restart_game()            
 
 if __name__ == "__main__":
